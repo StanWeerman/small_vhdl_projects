91f165a7bd90d6553fcfb798a73f2be3498e65b8
StanWeerman <stanweerman@gmail.com>
2026-03-09 01:38:51 -0700

    I2C Testbench work

--- i2c/i2c_tb.vhd
+++ i2c/i2c_tb.vhd
@@ -10,6 +10,14 @@ end i2c_tb;
 architecture i2c_tb of i2c_tb is
     signal clk: std_logic := '0';
     signal rst: std_logic := '1';
+    signal enable: std_logic;
+
+    signal rw: std_logic;
+    signal addr: std_logic_vector(6 downto 0);
+    signal byte_in: std_logic_vector(7 downto 0);
+    signal byte_out: std_logic_vector(7 downto 0);
+    signal busy, error: std_logic;
+    signal scl, sda: std_logic;
 
     constant ICLK: natural := 10000000;
     constant BCLK: time := 400000 ns;
@@ -24,13 +32,31 @@ architecture i2c_tb of i2c_tb is
 
     end procedure test_byte;
 begin
-    clk <= not clk after 50 ns;
+    clk <= not clk after 1 ns;
 
-
+    i2c_controller_inst: entity work.i2c_controller
+     generic map(
+        ICLK => 10000000,
+        BCLK => 400000
+    )
+     port map(
+        clk => clk,
+        rst => rst,
+        enable => enable,
+        rw => rw,
+        addr => addr,
+        byte_in => byte_in,
+        byte_out => byte_out,
+        busy => busy,
+        error => error,
+        scl => scl,
+        sda => sda
+    );
 
     testing: process is
     begin
-
+        rst <= '0';
+        wait for 500 ns;
 
         report "Tests Complete";
 
